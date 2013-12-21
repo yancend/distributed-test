75fc646c921d062141c262779abb8993f8228161
yancend <dongyancen@baidu.com>
2013-12-21 15:16:49 +0800

    最终展示结果页

--- display/displayRel.php
+++ display/displayRel.php
@@ -14,8 +14,29 @@
 
     <title></title>
     <script type="text/javascript" src="lib/jquery-1.5.1.js"></script>
+    <style type="text/css">
+        body {
+            font-family: 'Courier New', 'Heiti SC', 'Microsoft Yahei';
+            font-weight: bold;
+            font-size: 15px;
+        }
+        .tdClass{
+        }
+        .imgClass{
+            border: 1px solid #DDD;
+            /*padding: 10px;*/
+
+        }
+        .imgNameClass{
+        }
+        .imgsDiv{
+           margin-left:  8px;
+
+        }
+    </style>
 </head>
 <body>
+<table id="displayTable" style="width:100% ;display: none" ><tbody></tbody></table>
 </body>
 <?php
 echo  "<script type='text/javascript'>var urlsStr = '".$_GET['urls']."';</script>";
@@ -24,29 +45,44 @@ echo  "<script type='text/javascript'>var urlsStr = '".$_GET['urls']."';</script
 
 <script type="text/javascript">
     var urls = urlsStr.split(",");
-//    function addTr(startIndex){
-//        var tr = $("<tr></tr>");
-//        $("#displayTable").append(tr);
-//    }
-    //    var tdWidth = document.documentElement.scrollHeight/3;
-    //    var trNum = Math.floor(urls.length/3);
-    //    for(var i= 0;i<trNum;i++){
-    //        addTr(i*3);
-    //    }
+    function addTr(startIndex){
+        var tr = $("<tr></tr>").attr('width','100%');
+        for(var j= 0;j<3&&(startIndex+j<urls.length);j++){
+            var img = $("<img class=\"imgClass\" src=\""+urls[startIndex+j]+"\"  />" );
+            var imgName = urls[startIndex+j].split("/").slice(-1)[0];
 
-    var singleResultDiv = $("<div class='singleRel' style='text-align: center'></div>");
+            imgName = imgName.substr(0,imgName.length-4);
+            tr.append($("<td class='tdClass' valign='top'></td>").attr('width',tdWidth).append($("<div class='imgsDiv'></div>").append($("<p class='imgNameClass'>"+imgName+":</p>")).append(img).append($("<br/>"))));
 
-
-    var imgsDiv = $("<div class='imgsDiv'></div>");
-
-    //把该case的图片放到对应div
-    for(var i=0;i< urls.length;i++){
-        imgsDiv.append($("<img src=\""+urls[i]+"\"/>"));
-        imgsDiv.append($("<br/>"));
+        }
+        $("#displayTable").append(tr);
     }
-    singleResultDiv.append(imgsDiv);
-    //加到总的展示div
-    document.body.appendChild(singleResultDiv[0]);
+        var tdWidth = Math.floor(document.documentElement.scrollWidth/3);
+        var trNum = Math.ceil(urls.length/3);
+        for(var i= 0;i<trNum;i++){
+            addTr(i*3);
+        }
+    $(window).load(function(){
+        var imgs = $("img");
+        for(var p =0;p<imgs.length;p++){
+            $(imgs[p]).css("width","100%");
+        }
+        $("#displayTable").attr('style','width:100% ;display: block')
+
+    });
+//    var singleResultDiv = $("<div class='singleRel' style='text-align: center'></div>");
+//
+//
+//    var imgsDiv = $("<div class='imgsDiv'></div>");
+//
+//    //把该case的图片放到对应div
+//    for(var i=0;i< urls.length;i++){
+//        imgsDiv.append($("<img src=\""+urls[i]+"\"/>"));
+//        imgsDiv.append($("<br/>"));
+//    }
+//    singleResultDiv.append(imgsDiv);
+//    //加到总的展示div
+//    document.body.appendChild(singleResultDiv[0]);
 
 
     /*var  s = "";
@@ -67,5 +103,5 @@ echo  "<script type='text/javascript'>var urlsStr = '".$_GET['urls']."';</script
      s += "\r\n你的屏幕设置是 "+ window.screen.colorDepth +" 位彩色";
      s += "\r\n你的屏幕设置 "+ window.screen.deviceXDPI +" 像素/英寸";*/
 </script>
-<table id="displayTable" style="width:100%"><tbody></tbody></table>
+
 </html>
